77dff430a2e098c1d3c432f9dff5b5dea497216d
Sergiy <sergiyvergun01@gmail.com>
2022-03-23 14:40:51 +0100

    Implement jobs listing and edition.

---
+++ lib/app/home/jobs/job_list_tile.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class JobsListTile extends StatelessWidget {
+  const JobsListTile({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/app/home/jobs/jobs_page.dart
+++ lib/app/home/jobs/jobs_page.dart
@@ -1,14 +1,21 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:time_tracker_app/app/home/models/job.dart';
 import 'package:time_tracker_app/common_widgets/show_alert_dialog.dart';
+import 'package:time_tracker_app/common_widgets/show_exception_alert_dialog.dart';
 import 'package:time_tracker_app/services/auth.dart';
 import 'package:time_tracker_app/services/database.dart';
 
 class JobsPage extends StatelessWidget {
   Future<void> _createJob(BuildContext context) async {
-    var database = Provider.of<Database>(context, listen: false);
-    await database.createJob(Job(name: 'Blogging', ratePerHour: 10) );
+    try {
+      var database = Provider.of<Database>(context, listen: false);
+      await database.createJob(Job(name: 'Blogging', ratePerHour: 10));
+    } on FirebaseException catch (e) {
+      showExceptionAlertDialog(context,
+          title: 'Operation failed', exception: e);
+    }
   }
 
   @override
@@ -35,6 +42,27 @@ class JobsPage extends StatelessWidget {
             ),
           ),
         ],
+      ),
+      body: StreamBuilder<List<Job>>(
+        stream: context.read<Database>().jobsStream(),
+        builder: (BuildContext context, snapshot) {
+          if (snapshot.hasData) {
+            final jobs = snapshot.data;
+            return ListView.builder(
+                itemCount: jobs.length,
+                itemBuilder: (context, int index) {
+                  return Text(jobs.elementAt(index).name);
+                });
+          }
+          if (snapshot.hasError) {
+            return Center(child: Text('An error occurred'));
+          }
+          return Center(
+              child: Text(
+            'No Jobs',
+            style: Theme.of(context).textTheme.bodyText1,
+          ));
+        },
       ),
     );
   }
